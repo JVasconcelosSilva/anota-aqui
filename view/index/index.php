ca65e56a37ee9ad76354423cefea0de14b7fb355
Yan <yan.i.andrade@hotmail.com>
2020-10-27 11:08:58 -0300

    Front - Tela Index
    Falta Logo, texto quem somos e footer
    Todos em tratativas

--- view/index/index.php
+++ view/index/index.php
@@ -6,15 +6,95 @@ include '../_header/header.php';
 <html lang="pt-br">
 
 <head>
+
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="estiloIndex.css" type="text/css">
+<link rel="stylesheet" href="fontes/font-awesome.min.css">
+
+<script src="https://kit.fontawesome.com/54f9cce8ca.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+<title>Index</title>
+
+</head>
+
     <!-- Referência da folha de estilo do cabeçalho -->
     <link rel="stylesheet" href="../_header/header.css">
     <title>Index</title>
 </head>
 
 <body>
-    <!-- O index terá um navbar individual -->
-    <h1>test</h1>
-    <h4>test</h4>
+    <header class="header">  
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <img id="imghome" src="Imagens/Anota.png" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="hero-text text-center">
+                    <h1> Anota Aqui </h1>
+                </div>
+
+                <div class="hero-text text-center">
+                    <h3> Controle seu jogo ou alguém controlará por voce! </h4>
+                </div>
+            </div>
+        </div>
+    </header>
+
+<!----------------------------------------Quem Somos------------------------------------------>
+<br>
+        <div class="container-fluid">
+            <h1 class="text-center text-uppercase"> Quem Somos </h1>
+        </div>
+        <div class="col-md">
+        <br>
+            <p class="description" id="somos"> 
+               texto texto texto texto texto texto texto 
+               <br>
+               texto texto texto texto texto texto texto 
+               <br>
+               texto texto texto texto texto texto texto 
+            </p>
+        </div>  
+    <br>
+<!---------------------------------------------Atletas---+------------------------------------------------->
+<section class="testimonials"> 
+            <div class="conatiner">
+                <div class="wrap">
+                    
+                    <div class="box one">
+                        <div class="date">
+                        </div>
+                        <h1>CRIE</h1>
+                </div>
+                    
+                <div class="box two">
+                    <div class="date">
+                    </div>
+                    <H1>ANOTE</H1>
+                </div>
+	
+                <div class="box three">
+                    <div class="date">
+                    </div>
+                    <h1>COMPARTILHE</h1>
+                </div>
+            </div>
+    </section>
+</section>
 </body>
 
 </html>
